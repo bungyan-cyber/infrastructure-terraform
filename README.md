999112685757f41f9f2b5abba215d9d829969f12
Dian Pratama <dpratama2@linknetid.onmicrosoft.com>
2024-06-22 01:48:57 +0700

    push project IaC

---
+++ README.md
@@ -0,0 +1,22 @@
+# Infrastructure as Code with Terraform
+
+This project sets up a basic infrastructure on AWS using Terraform.
+
+## Requirements
+
+- Terraform
+- AWS Account
+
+## Infrastructure Components
+
+- A VPC
+- One public subnet
+- An EC2 instance running Ubuntu
+- An S3 bucket
+
+## Steps to Deploy the Infrastructure
+
+1. **Clone the Repository**:
+   ```bash
+   git clone git@github.com:yourusername/infrastructure-terraform.git
+   cd infrastructure-terraform
---
+++ main.tf
@@ -0,0 +1,73 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  key_name      = var.key_name
+
+  tags = {
+    Name = "web_instance"
+  }
+
+  user_data = <<-EOF
+            #!/bin/bash
+            apt-get update -y
+            apt-get upgrade -y
+            apt-get install -y nginx
+            EOF
+}
+
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-bucket-12345"
+  acl    = "private"
+
+  tags = {
+    Name = "my_bucket"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
